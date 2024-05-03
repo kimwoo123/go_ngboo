fca7b2c7481206ebc26751750afb113e21e7a523
kim woo seok <rladntjr4@gmail.com>
2024-05-04 00:23:58 +0900

    Feat: split bucket

--- crawl/main.go
+++ crawl/main.go
@@ -32,6 +32,21 @@ func tls_transport() *http.Transport {
 	return result
 }
 
+type noteLayer struct {
+	topNotes    []string
+	middleNotes []string
+	baseNotes   []string
+}
+
+type metaData struct {
+	name       string
+	brand      string
+	accordList []string
+	pros       []string
+	cons       []string
+	notes      noteLayer
+}
+
 func main() {
 	rawURL := "https://www.fragrantica.com/perfume/Tom-Ford/Tobacco-Vanille-1825.html"
 
@@ -51,10 +66,48 @@ func main() {
 		fmt.Println("Request URL: ", r.Request.URL, "\nError: ", err)
 	})
 
+	var m metaData
 	// // Set up callbacks to handle scraping events
-	c.OnHTML("h1[itemprop='name'].text-center.medium-text-left", func(e *colly.HTMLElement) {
-		// Print the scraped data
-		fmt.Printf("Text: %s\n", e.Text)
+	c.OnHTML("div[id='main-content']", func(e *colly.HTMLElement) {
+
+		fmt.Printf("NAME\n")
+		e.ForEach("h1[itemprop='name'].text-center.medium-text-left", func(_ int, el *colly.HTMLElement) {
+			fmt.Println(el.Text)
+			m.name = el.Text
+		})
+
+		fmt.Printf("ACCORD\n")
+		e.ForEach("div.accord-bar", func(_ int, el *colly.HTMLElement) {
+			fmt.Println(el.Text)
+			m.accordList = append(m.accordList, el.Text)
+		})
+
+		fmt.Printf("PROS\n")
+		e.ForEach("img[src='/ndimg/Pros_icon.svg']", func(_ int, el *colly.HTMLElement) {
+			fmt.Println(el.Text)
+			m.pros = append(m.pros, el.Text)
+		})
+
+		fmt.Printf("NOTE\n")
+		e.ForEach("div[id='pyramid']", func(_ int, el *colly.HTMLElement) {
+			var bucket *[]string
+			el.ForEach("/div[1]", func(_ int, subEl *colly.HTMLElement) {
+				fmt.Println(subEl.Text)
+				switch {
+				case subEl.Text == "Top Notes":
+					bucket = &m.notes.topNotes
+				case subEl.Text == "Middle Notes":
+					bucket = &m.notes.middleNotes
+				case subEl.Text == "Base Notes":
+					bucket = &m.notes.baseNotes
+				case bucket != nil:
+					*bucket = append(*bucket, subEl.Text)
+				}
+			})
+			fmt.Println(m.notes.topNotes)
+			fmt.Println(m.notes.middleNotes)
+			fmt.Println(m.notes.baseNotes)
+		})
 	})
 
 	// Visit the URL and start scraping
@@ -72,7 +125,8 @@ func main() {
 
 // 장점
 /*
- <div class="cell small-12 medium-6" style="border: 1px solid rgb(207, 249, 207); border-radius: 35px 0px; box-shadow: rgba(216, 216, 216, 0.73) -11px 6px 13px -2px; font-size: smaller;"><div style="display: flex; justify-content: center; margin-top: 0.4rem; gap: 5px;"><img src="/ndimg/Pros_icon.svg" alt="Pros" style="width: 1.5rem; height: 1.5rem;"><h4 class="header" style="background: linear-gradient(to right, rgb(108, 143, 109), rgb(207, 249, 207)) text; color: rgb(131, 166, 196);">
+ <div class="cell small-12 medium-6" style="border: 1px solid rgb(207, 249, 207); border-radius: 35px 0px; box-shadow: rgba(216, 216, 216, 0.73) -11px 6px 13px -2px; font-size: smaller;"><div style="display: flex; justify-content: center; margin-top: 0.4rem; gap: 5px;"><img src="/ndimg/Pros_icon.svg" alt="Pros" style="width: 1.5rem; height: 1.5rem;">
+ <h4 class="header" style="background: linear-gradient(to right, rgb(108, 143, 109), rgb(207, 249, 207)) text; color: rgb(131, 166, 196);">
 */
 
 //노트
